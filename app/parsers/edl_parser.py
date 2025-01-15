cc4b555c82fd1fbc34f8aa6120965b61b52675b2
Zach Domke <zdomke@slac.stanford.edu>
2025-01-14 16:24:34 -0800

    ENH: Parse screen properties

--- app/parsers/edl_parser.py
+++ app/parsers/edl_parser.py
@@ -2,6 +2,7 @@ import re
 
 
 class EDLAbstractObject:
+    """EDL Abstract Object class represents an abstract object in .edl files"""
     def __init__(self, x: int = None, y: int = None, width: int = None, height: int = None):
         self.x = x
         self.y = y
@@ -11,11 +12,13 @@ class EDLAbstractObject:
 
 
 class EDLScreenProperties(EDLAbstractObject):
+    """EDL Screen Properties class represents the screen properties in .edl files"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
 class EDLGroup(EDLAbstractObject):
+    """EDL Group class represents a group in .edl files"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.objects = []
@@ -28,6 +31,7 @@ class EDLGroup(EDLAbstractObject):
 
 
 class EDLObject(EDLAbstractObject):
+    """EDL Object class represents an object in .edl files"""
     def __init__(self, name: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = name
@@ -44,8 +48,26 @@ class EDLFileParser:
         with open(file_path, "r") as file:
             self.text = file.read()
 
+        self.screen_properties = None
+        self.screen_properties_end = 0
         self.groups = []
         self.objects = []
 
-    def parse_objects(self):
+        self.parse_screen_properties()
+        self.parse_objects_and_groups(self.text[self.screen_properties_end:])
+
+    def parse_screen_properties(self):
+        match = self.screen_prop_pattern.search(self.text)
+        if match:
+            screen_prop_text = match.group(1)
+            self.screen_properties_end = match.end()
+
+            x = int(re.search(r"x (\d+)", screen_prop_text).group(1))
+            y = int(re.search(r"y (\d+)", screen_prop_text).group(1))
+            width = int(re.search(r"w (\d+)", screen_prop_text).group(1))
+            height = int(re.search(r"h (\d+)", screen_prop_text).group(1))
+
+            self.screen_properties = EDLScreenProperties(x, y, width, height)
+
+    def parse_objects_and_groups(self, text):
         pass
