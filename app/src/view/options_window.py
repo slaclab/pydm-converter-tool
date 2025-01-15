3513bcbb4a954b3e05be3a459e3718a1ec75d1bc
Alexander Ng <ang@slac.stanford.edu>
2025-01-15 14:45:18 -0800

    options window accesses options model

--- app/src/view/options_window.py
+++ app/src/view/options_window.py
@@ -6,11 +6,20 @@ Attaches additional functionality to the options window view
 
 from os import path
 from pydm import Display
+from model.options_model import OptionsModel
 
 
 class OptionsWindow(Display):
-    def __init__(self, parent=None, args=None, macros=None, ui_filename=None):
+    def __init__(
+        self,
+        options_model: OptionsModel,
+        parent=None,
+        args=None,
+        macros=None,
+        ui_filename=None,
+    ):
         super(OptionsWindow, self).__init__(parent, args, macros, ui_filename)
+        self.options_model = options_model
         self.setup_ui()
 
     def ui_filename(self):
