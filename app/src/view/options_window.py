e7669e0ab20892c048a56a6f6ade8d96cf58ba8d
Alexander Ng <ang@slac.stanford.edu>
2025-01-16 17:03:22 -0800

    Options window can't be a display, also need to reevaluate how it will be used

--- app/src/view/options_window.py
+++ app/src/view/options_window.py
@@ -5,29 +5,21 @@ Attaches additional functionality to the options window view
 """
 
 from os import path
-from pydm import Display
+from qtpy.QtWidgets import QWidget
 from model.options_model import OptionsModel
 
 
-class OptionsWindow(Display):
+class OptionsWindow(QWidget):
     def __init__(
         self,
         options_model: OptionsModel,
         parent=None,
-        args=None,
-        macros=None,
-        ui_filename=None,
     ):
-        super(OptionsWindow, self).__init__(parent, args, macros, ui_filename)
+        super().__init__()
         self.options_model = options_model
         self.setup_ui()
 
-    def ui_filename(self):
-        return "options_window.ui"
-
-    def ui_filepath(self):
-        return path.join(path.dirname(path.realpath(__file__)), self.ui_filename())
-
     def setup_ui(self):
-        self.setFixedWidth(600)
-        self.setFixedHeight(300)
+        # self.setFixedWidth(600)
+        # self.setFixedHeight(300)
+        pass
