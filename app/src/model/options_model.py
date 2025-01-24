854def5951ee406b3ac27a1b9913ed09dfde8db5
Alexander Ng <ang@slac.stanford.edu>
2025-01-24 15:29:06 -0800

    ENH: Add addtional options to options window, including behaviors to validate, read and write to file

--- app/src/model/options_model.py
+++ app/src/model/options_model.py
@@ -11,7 +11,12 @@ import os
 
 class OptionsModel:
     def __init__(self):
+        # General options
         self.output_folder = None
+
+        # EDM options
+        self.edm_override_def_colors = False
+        self.edm_custom_colors_path = "/colors.list"
 
     def get_options_from_file(self, filepath: str = "./app/src/options.json") -> None:
         """Retrieves GUI options from file, if it exists. Otherwise creates options file.
@@ -30,9 +35,18 @@ class OptionsModel:
         with open(filepath, "r") as json_file:
             try:
                 data = json.load(json_file)
+                # General options
                 output_folder = data.get("output_folder", None)
                 if output_folder is not None and os.path.exists(output_folder):
                     self.output_folder = output_folder
+
+                # EDM options
+                edm_override_def_colors = data.get("edm_override_def_colors", None)
+                if edm_override_def_colors is not None:
+                    self.edm_override_def_colors = edm_override_def_colors
+                edm_custom_colors_path = data.get("edm_custom_colors_path", None)
+                if edm_custom_colors_path is not None:
+                    self.edm_custom_colors_path = edm_custom_colors_path
             except json.JSONDecodeError:
                 logging.error(f"Invalid JSON in file: {filepath}.")
 
@@ -44,8 +58,15 @@ class OptionsModel:
         """
 
         data = {}
+        # General options
         if self.output_folder is not None:
             data["output_folder"] = self.output_folder
+
+        # EDM options
+        if self.edm_override_def_colors is not False:
+            data["edm_override_def_colors"] = self.edm_override_def_colors
+        if self.edm_custom_colors_path is not None:
+            data["edm_custom_colors_path"] = self.edm_custom_colors_path
 
         with open(filepath, "w") as json_file:
             json.dump(data, json_file, indent=4)
