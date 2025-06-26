9e3047bbc01485a54fccff321304a1d6f0c22011
Logan Somoza <lsomoza@dev-srv09.slac.stanford.edu>
2025-06-26 10:56:18 -0700

    added setup.py to allow for calling pydmconverter executable directly

---
+++ setup.py
@@ -0,0 +1,15 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pydmconverter',
+    version='0.1',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'pydmconverter = pydmconverter.__main__:main',
+        ],
+    },
+    install_requires=[]  # Add dependencies if needed
+)
