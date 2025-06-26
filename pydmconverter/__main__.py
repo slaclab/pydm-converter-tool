dbfc1a41226b520fd0ece310a056d0edcf627da2
Logan Somoza <lsomoza@dev-srv09.slac.stanford.edu>
2025-06-26 13:19:33 -0700

    cleaned up cli code to use pathlib instead of strings

--- pydmconverter/__main__.py
+++ pydmconverter/__main__.py
@@ -6,6 +6,7 @@ import os
 #import edm.converter
 from pydmconverter.edm.converter import convert
 import glob
+from pathlib import Path
 
 def run_gui():
     """
@@ -19,57 +20,54 @@ def run_cli(args):
     run PyDMConverter through command line"
     """
     print("Running CLI with arguments:", args)
-    input_file: str = args.input_file
-    output_file: str = args.output_file #maybe confusing input and output file can also be directories
+    input_path: Path = Path(args.input_file)
+    output_path: Path = Path(args.output_file)
     input_file_type: str = args.output_type
-    if os.path.isfile(args.input_file): 
-        if len(output_file) < 3 or output_file[-3:] != '.ui':
-            output_file += ".ui"
-        convert(input_file, output_file)
-    elif os.path.isdir(input_file):
-        if not os.path.isdir(output_file):
-            os.makedirs(output_file)
-        if input_file_type[0] != '.': #prepending . #technically redundant
-            input_file_type = '.' + input_file_type
-        search_pattern = os.path.join(input_file, '*' + input_file_type)
-        inputted_files = glob.glob(search_pattern)
-        for file in inputted_files:
-            output_file_name = get_output_file_name(file, output_file)
-            convert(file, output_file_name)
-    else:
-        raise ValueError(f"Input path '{input_file}' is neither a file nor a directory.")
 
-def get_output_file_name(file: str, output_file: str):
-    input_file_name = file.split('/')[-1]
-    #input_file_name = ".".join(input_file_name.split['.'][:-1]) #this works for all file types but may have issues if file does not end with a .type
-    if len(input_file_name) >= 4 and input_file_name[-4:] == '.edl':
-        input_file_name = input_file_name[:-4]
-    input_file_name += '.ui'
-    return output_file + '/' + input_file_name
+    if input_path.is_file():
+        if output_path.suffix != '.ui':
+            output_path = output_path.with_suffix('.ui')
+        convert(str(input_path), str(output_path))
+    elif input_path.is_dir():
+        if input_file_type[0] != '.': #prepending . so it will not pick up other file types with same suffix
+            input_file_type = '.' + input_file_type
+        output_path.mkdir(parents=True, exist_ok=True)
+        convert_files_in_folder(input_path, output_path, input_file_type)
+    else:
+        raise ValueError(f"Input path '{str(input_path)}' is neither a file nor a directory.") #moved to earlier so likely can remove
+    
+def convert_files_in_folder(input_path: Path, output_path: Path, input_file_type: str):
+    inputted_files = input_path.glob(f'*{input_file_type}')
+    for file in inputted_files:
+        output_file_name = get_output_file_name(file, output_path)
+        convert(file, output_file_name)
+
+def get_output_file_name(file: Path, output_path: Path):
+    return output_path / (file.stem + '.ui')
 
 def check_parser_errors(args: object, parser: argparse.ArgumentParser):
     if not args.input_file or not args.output_file: #techincally may be folders instead but this should still work for that
         parser.error("Must input two files")
-    #if not os.path.isfile(args.input_file) or not os.path.isdir(args.input_file):
-    #    parser.error("Invalid input file: Must input a valid file or directory")
-    """if os.path.isfile(args.input_file):
-        file_dir = "/".join(args.input_file.split('/')[:-1])
-        if not os.path.isdir(file_dir):
-            parser.error("Invalid output file directory")
-    elif os.path.isdir(args.input_file) and not os.path.isdir(args.output_file): #this assumes that the user does not want to add the files to the root
-        parser.error("Invalid output directory")
-    else:
-        parser.error("Invalid input file: Must input a valid file or directory")"""
+    if not os.path.isfile(args.input_file) and not os.path.isdir(args.input_file):
+        parser.error(f"Input path '{args.input_file}' is neither a file nor a directory.")
 
 def create_new_directories(args: object, parser: argparse.ArgumentParser):
-    if os.path.isfile(args.input_file):
+    input_path = Path(args.input_file)
+    output_path = Path(args.output_file)
+
+    if input_path.is_file():
+        file_dir = output_path.parent
+        file_dir.mkdir(parents=True, exist_ok=True)
+        print('file_dir', file_dir)
+    elif input_path.is_dir() and not output_path.exists():
+        output_path.mkdir(parents=True, exist_ok=True)
+    """if os.path.isfile(args.input_file):
         file_dir = "/".join(args.output_file.split('/')[:-1])
-        print(file_dir)
         if not os.path.isdir(file_dir) and file_dir:
             os.makedirs(file_dir)
         print('file_dir', file_dir)
     if os.path.isdir(args.input_file) and not os.path.isdir(args.output_file) and args.output_file: #maybe unnecesary
-        os.makedirs(args.output_file)
+        os.makedirs(args.output_file)"""
 
 def main():
     import sys
