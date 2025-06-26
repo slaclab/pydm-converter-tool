1be001cdcaa139a2ed9cab953e26e2a409436925
Logan Somoza <lsomoza@dev-srv09.slac.stanford.edu>
2025-06-26 15:50:17 -0700

    added basic override prevention

--- pydmconverter/__main__.py
+++ pydmconverter/__main__.py
@@ -23,26 +23,36 @@ def run_cli(args: argparse.Namespace) -> None:
     input_path: Path = Path(args.input_file)
     output_path: Path = Path(args.output_file)
     input_file_type: str = args.output_type
+    override: bool = args.override
 
     if input_path.is_file():
         if output_path.suffix != ".ui":
             output_path = output_path.with_suffix(".ui")
+        if output_path.is_file() and not override:
+            raise FileExistsError(f"Output file '{output_path}' already exists. Use --override or -o to overwrite it.")
         convert(str(input_path), str(output_path))
     else:
         if input_file_type[0] != ".":  # prepending . so it will not pick up other file types with same suffix
             input_file_type = "." + input_file_type
         output_path.mkdir(parents=True, exist_ok=True)
-        convert_files_in_folder(input_path, output_path, input_file_type)
+        convert_files_in_folder(input_path, output_path, input_file_type, override)
 
 
 def convert_files_in_folder(
-    input_path: Path, output_path: Path, input_file_type: str
+    input_path: Path, output_path: Path, input_file_type: str, override: bool
 ) -> int:  # outputs the amount of files found in this directory and subdirectories
+    # print("here")
     files_found = 0
-    inputted_files = input_path.glob(f"*{input_file_type}")
-    inputted_length = len(list(inputted_files))
+    inputted_files = list(input_path.glob(f"*{input_file_type}"))
+    inputted_length = len(inputted_files)
+    # print(inputted_length)
     for file in inputted_files:
         output_file_name = get_output_file_name(file, output_path)
+        output_file_path = Path(output_file_name)
+        # print("now here")
+        # print('blah', output_file_path.is_file(), override)
+        if output_file_path.is_file() and not override:
+            raise FileExistsError(f"Output file '{output_path}' already exists. Use --override or -o to overwrite it.")
         convert(file, output_file_name)
 
     subdirectories = [item for item in input_path.iterdir() if item.is_dir()]
@@ -78,15 +88,12 @@ def create_new_directories(args: argparse.Namespace, parser: argparse.ArgumentPa
 def main() -> None:
     print("Args:", sys.argv)
     parser: argparse.ArgumentParser = argparse.ArgumentParser()
-    """parser.add_argument(
-        "--cli", action="store_true", help="If provided, run in command-line (CLI) mode instead of GUI."
-    )"""
     parser.add_argument("input_file", nargs="?", metavar="FILE")
     parser.add_argument("output_file", nargs="?", metavar="FILE")
     parser.add_argument("output_type", nargs="?", metavar="FILE TYPE")
+    parser.add_argument("--override", "-o", action="store_true", help="Override the output file if it already exists")
     args: argparse.Namespace = parser.parse_args()
 
-    # if args.input_file:
     if args.input_file:
         check_parser_errors(args, parser)
         create_new_directories(args, parser)
